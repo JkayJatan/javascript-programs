5eb1f10ecb1dfcfa67ce7dae6fcf5fd79b6b62a1
JkayJatan <jatankumarjk806@gmail.com>
2025-09-09 23:45:39 +0530

    Add program to generate multiplication table

---
+++ Basics/17_multiplicarion_table.js
@@ -0,0 +1,6 @@
+let num = 5;   // change the number as you like
+let range = 10;  // how far the table should go
+
+for (let i = 1; i <= range; i++) {
+    console.log(num + " x " + i + " = " + (num * i));
+}
